353e894cd0444919ed2818f8cd09032d4f8fc497
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-01-29 04:55:27 +0530

    Update file 28/01/2025, 17:25:27

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        int[]  res = new int[2];
+
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(target-nums[i])){
-                res[0] = map.get(target-nums[i]);
-                res[1] = i;
-                return res;
+                return new int[]{map.get(target-nums[i]),i};
             }
             else{
                 map.put(nums[i],i);
             }
         }
-        
-        return res;
-        
+
+        return new int[]{-1,-1};
     }
 }
