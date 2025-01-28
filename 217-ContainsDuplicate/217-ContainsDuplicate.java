118678fbdd0cd8c1c48b558c211b1a8381b51113
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-01-29 01:42:58 +0530

    Create file

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        int i=0;
+
+        while(i<nums.length){
+            if(set.contains(nums[i])){
+                return true;
+            }
+
+            set.add(nums[i]);
+            i++;
+        }
+
+        return false;
+    }
+}
