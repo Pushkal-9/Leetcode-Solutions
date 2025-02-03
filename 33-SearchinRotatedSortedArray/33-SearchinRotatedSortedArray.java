6e2cb1a5601fd3bddd88cd65d65430412c75d8fa
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-02-04 03:49:39 +0530

    Update file 03/02/2025, 16:19:39

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,7 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length-1;
+        int low = 0, high = nums.length-1;
 
         while(low<=high){
             int mid = low + (high-low)/2;
@@ -9,16 +8,16 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<nums[mid]){
-                    high=mid-1;
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>target){
+                    high = mid-1;
                 }
                 else{
-                    low=mid+1;
+                    low = mid+1;
                 }
             }
             else{
-                if(nums[mid]<target && target<=nums[high]){
+                if(nums[high]>=target && nums[mid]<target){
                     low=mid+1;
                 }
                 else{
