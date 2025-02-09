da02acd5ee59e7601d6cd430cbbfd2a274c58d19
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-02-08 20:27:46 -0600

    File updated at 08/02/2025, 20:27:46

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        HashSet<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
-        HashSet<List<Integer>> set = sums(candidates,target,res,current,0,0);
-        return new ArrayList<>(set);
+
+        return sums(candidates,target,res,current,0,0);
     }
 
-    public HashSet<List<Integer>> sums(int[] arr, int target, HashSet<List<Integer>> res, List<Integer> current,int index, int sum){
+    public List<List<Integer>> sums(int[] arr, int target, List<List<Integer>> res, List<Integer> current,int index, int sum){
         if(index>=arr.length || sum>target){
             return res;
         }
 
         if(sum==target){
-            res.add(current);
+            res.add(new ArrayList<>(current));
             return res;
         }
 
@@ -22,8 +22,6 @@ class Solution {
         newCurrent.add(arr[index]);
 
         res = sums(arr,target,res,newCurrent,index,sum+arr[index]);
-
-        res = sums(arr,target,res,newCurrent,index+1,sum+arr[index]);
 
         return res;
     }
