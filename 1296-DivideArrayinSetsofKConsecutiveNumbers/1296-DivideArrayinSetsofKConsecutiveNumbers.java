1aeeca2dc7428a54707cde8a4943d194202850de
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-02-18 17:03:09 -0600

    Create file

---
+++ 1296-DivideArrayinSetsofKConsecutiveNumbers/1296-DivideArrayinSetsofKConsecutiveNumbers.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int groupSize) {
+     if((hand.length)%groupSize!=0){
+            return false;
+        }
+
+        HashMap<Integer,Integer> count = new HashMap<>();
+        SortedSet<Integer> set = new TreeSet<>();
+
+        for(int i : hand){
+            count.put(i, count.getOrDefault(i,0)+1);
+            set.add(i);
+        }
+
+        while(!set.isEmpty()){
+            int h = set.first();
+
+            for(int i=0;i<groupSize;i++){
+                if(count.containsKey(h+i)){
+                    count.put(h+i,count.get(h+i)-1);
+                    if(count.get(h+i)==0){
+                        count.remove(h+i);
+                        set.remove(h+i);
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return true;
+        
+    }
+}
