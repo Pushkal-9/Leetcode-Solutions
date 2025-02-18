bea757b5a3acbed650602d86d32b367e336f563f
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-02-18 17:41:23 -0600

    File updated at 18/02/2025, 17:41:22

--- 763-PartitionLabels/763-PartitionLabels.java
+++ 763-PartitionLabels/763-PartitionLabels.java
@@ -1,69 +1,26 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        HashMap<Character,Integer> first = new HashMap<>();
+
         HashMap<Character,Integer> last = new HashMap<>();
 
         for(int i=0;i<s.length();i++){
-            if(!first.containsKey(s.charAt(i))){
-                first.put(s.charAt(i),i);
-                last.put(s.charAt(i),i);
-            }
-            else{
-                last.put(s.charAt(i),i);
-            }
+            last.put(s.charAt(i),i);
         }
-
-        List<Point> list = new ArrayList<>();
-
-        for(char ch : first.keySet()){
-            list.add(new Point(first.get(ch),true));
-        }
-
-        for(char ch : last.keySet()){
-            list.add(new Point(last.get(ch),false));
-        }
-
-        Collections.sort(list, (a,b) -> {
-            if(a.value!=b.value){
-                return a.value-b.value;
-            }
-
-            return Boolean.compare(b.isStart,a.isStart);
-        });
 
         int start=0;
         int end = 0;
 
-        int count = 0;
-
         List<Integer> res = new ArrayList<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, last.get(s.charAt(i))); 
 
-        for(Point point : list){
-            if(point.isStart){
-                if(count==0)
-                    start=point.value;
-                count++;
-            }
-            else{
-                count--;
-                if(count==0){
-                    end=point.value;
-                    res.add(end-start+1);
-                }
+            if (i == end) { 
+                res.add(end - start + 1);
+                start = i + 1;
             }
         }
 
         return res;
-    }
-}
-
-class Point{
-    int value;
-    boolean isStart;
-
-    public Point(int value, boolean isStart){
-        this.value=value;
-        this.isStart=isStart;
     }
 }
