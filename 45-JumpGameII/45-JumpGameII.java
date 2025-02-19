77f37977532bb17d7bd94b9013765f8320361102
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-02-18 22:59:54 -0600

    File updated at 18/02/2025, 22:59:54

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,17 +1,4 @@
-class Solution {
-    public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0]=0;
-
-        for(int i=0;i<nums.length;i++){
-            for(int j=1;j<=nums[i] && i+j<nums.length;j++){
-                dp[i+j]= Math.min(dp[i+j],dp[i]+1);
-            }
-            
-        }
-
-        return dp[nums.length-1];
-    }
-}
+// why we need i < length-1 ?
+//  I think it's when we is at index 0, we don`t count the index.
+// is it  right ??
+for (int i = 0; i < A.length - 1; i++) {
