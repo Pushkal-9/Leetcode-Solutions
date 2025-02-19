f7e4a38b03983f387788b1fc8e7e45f5fafed794
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-02-18 22:51:05 -0600

    File updated at 18/02/2025, 22:51:04

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,16 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length+1];
+        int[] dp = new int[nums.length];
 
         Arrays.fill(dp,Integer.MAX_VALUE);
         dp[0]=0;
 
         for(int i=0;i<nums.length;i++){
-            for(int j=1;j<=nums[i] && i+j<=nums.length;j++){
+            for(int j=1;j<=nums[i] && i+j<nums.length;j++){
                 dp[i+j]= Math.min(dp[i+j],dp[i]+1);
             }
             
-            //System.out.println(dp[i]+ " " + i);
         }
 
         return dp[nums.length-1];
