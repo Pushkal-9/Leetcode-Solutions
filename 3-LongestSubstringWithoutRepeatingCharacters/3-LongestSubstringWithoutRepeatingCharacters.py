bff7783707fbcfdc88ff06ea0ec840ca3ebc3e5f
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-08 18:13:48 -0500

    File updated at 08/04/2025, 18:13:48

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,4 +1,4 @@
-# Last updated: 08/04/2025, 18:13:38
+# Last updated: 08/04/2025, 18:13:48
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         start=0
@@ -7,7 +7,7 @@ class Solution:
         hash_set = set()
         maxLength=0
 
-        while(start<=end and end<len(s)):
+        while(end<len(s)):
             if s[end] not in hash_set:
                 hash_set.add(s[end])
                 maxLength=max(maxLength,end-start+1)
