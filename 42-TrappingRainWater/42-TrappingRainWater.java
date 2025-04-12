8937916cb744e2ee7154a0ec4fe642f79a42d09b
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-11 21:54:14 -0500

    File updated at 11/04/2025, 21:54:14

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,27 +1,28 @@
+// Last updated: 11/04/2025, 21:54:14
 class Solution {
     public int trap(int[] height) {
         int[] left = new int[height.length];
         int[] right = new int[height.length];
 
-        left[0]=0;
 
+        left[0]=0;
         for(int i=1;i<height.length;i++){
-            left[i]= Math.max(height[i-1],left[i-1]);
+            left[i]= Math.max(left[i-1],height[i-1]);
         }
+
 
         right[height.length-1]=0;
 
-        for(int j=height.length-2;j>=0;j--){
-            right[j] = Math.max(height[j+1],right[j+1]);
+        for(int i=height.length-2;i>=0;i--){
+            right[i]=Math.max(height[i+1],right[i+1]);
         }
 
         int total = 0;
 
-        for(int i=1;i<height.length-1;i++){
+        for(int i=0;i<height.length;i++){
             int min = Math.min(left[i],right[i]);
-
-            //System.out.println(left[i] + " "+ right[i]);
-            total = total + (min>height[i]?min-height[i]:0);
+            if(min>height[i])
+                total = total + min-height[i];
         }
 
         return total;
