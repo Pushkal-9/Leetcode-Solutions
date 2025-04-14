4a7df7eef1fc967e0680dcf991325aa370f3099a
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-14 16:38:03 -0500

    File updated at 14/04/2025, 16:38:02

--- 1209-RemoveAllAdjacentDuplicatesinStringII/1209-RemoveAllAdjacentDuplicatesinStringII.java
+++ 1209-RemoveAllAdjacentDuplicatesinStringII/1209-RemoveAllAdjacentDuplicatesinStringII.java
@@ -1,11 +1,12 @@
-// Last updated: 14/04/2025, 16:37:21
+// Last updated: 14/04/2025, 16:38:02
 class Solution {
     public String removeDuplicates(String s, int k) {
         Stack<Pair> stack = new Stack<>();
 
         for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
             if(stack.isEmpty()){
-                stack.push(new Pair(s.charAt(i),1));
+                stack.push(new Pair(c,1));
             }
             else{
                 if(stack.peek().ch == s.charAt(i)){
@@ -15,7 +16,7 @@ class Solution {
                     }
                 }
                 else{
-                    stack.push(new Pair(s.charAt(i),1));
+                    stack.push(new Pair(c,1));
                 }
             }
         }
