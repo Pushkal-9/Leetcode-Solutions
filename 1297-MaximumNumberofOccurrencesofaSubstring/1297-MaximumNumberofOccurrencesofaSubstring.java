5b3bbb640b5739d888004ba3e5f1f832c7b614c5
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-13 20:35:39 -0500

    File updated at 13/04/2025, 20:35:39

--- 1297-MaximumNumberofOccurrencesofaSubstring/1297-MaximumNumberofOccurrencesofaSubstring.java
+++ 1297-MaximumNumberofOccurrencesofaSubstring/1297-MaximumNumberofOccurrencesofaSubstring.java
@@ -1,60 +1,46 @@
-// Last updated: 13/04/2025, 20:32:02
+// Last updated: 13/04/2025, 20:35:39
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
         int[] count = new int[26];
+        HashMap<String, Integer> map = new HashMap<>();
 
-        HashMap<String,Integer> map = new HashMap<>();
+        // Only loop for minSize (as per optimal logic)
+        int size = minSize;
+        int k = 0;
 
+        // Initial window
+        for (int i = 0; i < size; i++) {
+            char ch = s.charAt(i);
+            if (count[ch - 'a'] == 0) k++; // new unique char
+            count[ch - 'a']++;
+        }
 
-        for(int size=minSize;size<=minSize;size++){
-            count = new int[26];
-            int k = 0;
-            for(int i=0;i<size;i++){
-                char ch = s.charAt(i);
-                count[ch-'a']++;
-            }
+        if (k <= maxLetters) {
+            map.put(s.substring(0, size), 1);
+        }
 
-            for(int i=0;i<26;i++){
-                if(count[i]>0){
-                    k++;
-                }
-            }
+        // Sliding window
+        for (int i = 0; i < s.length() - size; i++) {
+            char out = s.charAt(i);
+            char in = s.charAt(i + size);
 
-            if(k<=maxLetters){
-                map.put(s.substring(0,size),1);
-                //System.out.println(s.substring(0,size));
-            }
+            count[out - 'a']--;
+            if (count[out - 'a'] == 0) k--; // one unique char removed
 
-            for(int i=0;i<s.length()-size;i++){
-                count[s.charAt(i)-'a']--;
-                count[s.charAt(i+size)-'a']++;
+            if (count[in - 'a'] == 0) k++; // new unique char coming in
+            count[in - 'a']++;
 
-
-                if(unique(count)<=maxLetters){
-                    String str = s.substring(i+1,i+size+1);
-                    map.put(str,map.getOrDefault(str,0)+1);
-                }
+            if (k <= maxLetters) {
+                String str = s.substring(i + 1, i + size + 1);
+                map.put(str, map.getOrDefault(str, 0) + 1);
             }
         }
 
         int max = 0;
-
-        for(int i : map.values()){
-            max=Math.max(i,max);
+        for (int val : map.values()) {
+            max = Math.max(max, val);
         }
 
         return max;
-    }
-
-    public int unique(int[] count){
-        int u = 0;
-        
-        for(int i : count){
-            if(i>0){
-                u++;
-            }
-        }
-
-        return u;
     }
 }
