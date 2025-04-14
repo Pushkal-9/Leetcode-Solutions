19c08e6af21f44284e4c3a6d90f40b74fe90a7d6
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-13 20:38:19 -0500

    File updated at 13/04/2025, 20:38:19

--- 1297-MaximumNumberofOccurrencesofaSubstring/1297-MaximumNumberofOccurrencesofaSubstring.java
+++ 1297-MaximumNumberofOccurrencesofaSubstring/1297-MaximumNumberofOccurrencesofaSubstring.java
@@ -1,44 +1,52 @@
-// Last updated: 13/04/2025, 20:35:39
+// Last updated: 13/04/2025, 20:38:19
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
         int[] count = new int[26];
-        HashMap<String, Integer> map = new HashMap<>();
 
-        // Only loop for minSize (as per optimal logic)
+        HashMap<String,Integer> map = new HashMap<>();
         int size = minSize;
-        int k = 0;
 
-        // Initial window
-        for (int i = 0; i < size; i++) {
-            char ch = s.charAt(i);
-            if (count[ch - 'a'] == 0) k++; // new unique char
-            count[ch - 'a']++;
-        }
-
-        if (k <= maxLetters) {
-            map.put(s.substring(0, size), 1);
-        }
-
-        // Sliding window
-        for (int i = 0; i < s.length() - size; i++) {
-            char out = s.charAt(i);
-            char in = s.charAt(i + size);
-
-            count[out - 'a']--;
-            if (count[out - 'a'] == 0) k--; // one unique char removed
-
-            if (count[in - 'a'] == 0) k++; // new unique char coming in
-            count[in - 'a']++;
-
-            if (k <= maxLetters) {
-                String str = s.substring(i + 1, i + size + 1);
-                map.put(str, map.getOrDefault(str, 0) + 1);
+            count = new int[26];
+            int k = 0;
+            for(int i=0;i<size;i++){
+                char ch = s.charAt(i);
+                count[ch-'a']++;
             }
-        }
+
+            for(int i=0;i<26;i++){
+                if(count[i]>0){
+                    k++;
+                }
+            }
+
+            if(k<=maxLetters){
+                map.put(s.substring(0,size),1);
+            }
+
+            for(int i=0;i<s.length()-size;i++){
+                count[s.charAt(i)-'a']--;
+
+
+                if(count[s.charAt(i)-'a']==0){
+                    k--;
+                }
+
+                if(count[s.charAt(i+size)-'a']==0){
+                    k++;
+                }
+                count[s.charAt(i+size)-'a']++;
+
+
+                if(k<=maxLetters){
+                    String str = s.substring(i+1,i+size+1);
+                    map.put(str,map.getOrDefault(str,0)+1);
+                }
+            }
 
         int max = 0;
-        for (int val : map.values()) {
-            max = Math.max(max, val);
+
+        for(int i : map.values()){
+            max=Math.max(i,max);
         }
 
         return max;
