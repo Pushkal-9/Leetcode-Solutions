d04446e95ed8383f9f73e1c69944a45d7eae442a
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-14 16:02:27 -0500

    File updated at 14/04/2025, 16:02:27

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.java
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -1,23 +1,54 @@
+// Last updated: 14/04/2025, 16:02:27
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n=matrix[0].length;
-        int m=matrix.length;
-        int i=0;
-        int j=n-1;
-        
-        while(i<m && j>=0){
-            if(matrix[i][j]==target){
+
+        int row = findRow(matrix,target);
+
+        int low = 0;
+        int high = matrix[0].length-1;
+
+        if (row < 0 || row >= matrix.length || matrix[row][0] > target) {
+            return false;
+        }
+
+        //System.out.println(row);
+
+
+        while(low<=high){
+            int mid = (high-low)/2 + low;
+
+            if(matrix[row][mid]==target){
                 return true;
             }
-            else if(matrix[i][j]<target){
-                i++;
+            else if(matrix[row][mid]>target){
+                high = mid-1;
             }
-            
-            else if(matrix[i][j]>target){
-                j--;
+            else{
+                low = mid+1;
             }
         }
         
         return false;
+    }
+
+    public int findRow(int[][] matrix, int target){
+        int low = 0;
+        int high = matrix.length-1;
+
+        while(low<=high){
+            int mid = (high-low)/2 + low;
+
+            if(matrix[mid][0]==target){
+                return mid;
+            }
+            else if(matrix[mid][0]>target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        return high;
     }
 }
