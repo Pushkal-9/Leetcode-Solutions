a0fbf8bd071615595d3d5e8b29bc8465b09d07eb
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-13 22:38:34 -0500

    File updated at 13/04/2025, 22:38:34

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,17 +1,20 @@
-public int maxProfit(int[] prices) {
-    if(prices.length<2) return 0;
-    int diff[] = new int[prices.length-1];
-    for(int i=1; i<prices.length; i++){
-        diff[i-1] = prices[i] - prices[i-1];
+// Last updated: 13/04/2025, 22:38:34
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+
+        int[] right = new int[prices.length];
+
+        right[prices.length-1]=0;
+
+        for(int i=right.length-2; i>=0 ;i--){
+            right[i]=Math.max(right[i+1],prices[i+1]);
+        }
+
+        for(int i=0;i<prices.length;i++){
+            max=Math.max(max,right[i]-prices[i]);
+        }
+
+        return max;
     }
-    return maxSubArray(diff);
-}
-public int maxSubArray(int[] nums) {
-    if(nums.length<1) return 0;
-    int preMax = 0, m = 0;
-    for(int i=0; i<nums.length; i++){
-        m = Math.max(m, preMax+nums[i]);
-        preMax = Math.max(0, preMax+nums[i]);
-    }
-    return m;
 }
