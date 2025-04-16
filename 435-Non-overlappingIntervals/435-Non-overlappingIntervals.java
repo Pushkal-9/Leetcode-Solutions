bb3d40930a9e4ef91076fc28b8bc30da462afb65
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-15 23:51:49 -0500

    File updated at 15/04/2025, 23:51:49

--- 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
+++ 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
@@ -1,22 +1,43 @@
+// Last updated: 15/04/2025, 23:51:49
+import java.util.*;
+
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int count = 0; 
+        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
 
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        int[] dp = new int[n];
+        dp[0] = 1;
 
-        int end = intervals[0][1];
+        for (int i = 1; i < n; i++) {
 
-        for(int i=1;i<intervals.length;i++){
-
-            if(end>intervals[i][0]){
-                end = Math.min(end,intervals[i][1]);
-                count++;
-            }
-            else{
-                end = intervals[i][1];
+            int prev = binarySearch(intervals, i);
+            if (prev != -1) {
+                dp[i] = Math.max(dp[i - 1], dp[prev] + 1);
+            } else {
+                dp[i] = Math.max(dp[i - 1], 1);
             }
         }
 
-        return count;
+        int maxNonOverlapping = dp[n - 1];
+        return n - maxNonOverlapping;
+    }
+
+    private int binarySearch(int[][] intervals, int curr) {
+        int low = 0, high = curr - 1;
+        int target = intervals[curr][0];
+        int res = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (intervals[mid][1] <= target) {
+                res = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return res;
     }
 }
