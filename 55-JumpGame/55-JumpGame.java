e36b2d0e5ecf98e57bbf45a736d4f57506ea4b9b
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-16 11:55:52 -0500

    File updated at 16/04/2025, 11:55:52

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,15 +1,14 @@
+// Last updated: 16/04/2025, 11:55:52
 class Solution {
     public boolean canJump(int[] nums) {
         int max=0;
 
         for(int i=0;i<nums.length;i++){
-            if(i>max){
-                return false;
+            if(max>=i){
+                max=Math.max(max,i+nums[i]);
             }
-
-            max=Math.max(max,nums[i]+i);
-            if(max>nums.length-1){
-                return true;
+            else{
+                return false;
             }
         }
 
