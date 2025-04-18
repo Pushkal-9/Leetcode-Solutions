bf759ce51afe3cad91a7bc45a8291c1689f2e2b0
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-18 18:30:16 -0500

    File updated at 18/04/2025, 18:30:16

--- 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.java
+++ 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.java
@@ -1,21 +1,14 @@
-// Last updated: 18/04/2025, 18:27:27
+// Last updated: 18/04/2025, 18:30:16
 class Solution {
+
     public int findTheWinner(int n, int k) {
-        List<Integer> list = new ArrayList<>();
+        return winnerHelper(n, k) + 1;
+    }
 
-        for(int i=1;i<=n;i++){
-            list.add(i);
+    private int winnerHelper(int n, int k) {
+        if (n == 1) {
+            return 0;
         }
-
-        int startIndex = 0;
-
-        while(list.size()>1){
-            int removalIndex = (startIndex + k - 1) % list.size();
-            list.remove(removalIndex);
-
-            startIndex = removalIndex;
-        }
-
-        return list.get(0);
+        return (winnerHelper(n - 1, k) + k) % n;
     }
 }
