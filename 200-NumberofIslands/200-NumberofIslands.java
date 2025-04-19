d8a606c0710537d64240236f43c5fde8f2a4f801
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-19 14:13:16 -0500

    File updated at 19/04/2025, 14:13:16

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,14 +1,16 @@
+// Last updated: 19/04/2025, 14:13:16
 class Solution {
     public int numIslands(char[][] grid) {
+        
         boolean[][] vis = new boolean[grid.length][grid[0].length];
 
         int count = 0;
 
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
+                if(!vis[i][j] && grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j,vis);
+                    dfs(grid,vis,i,j);
                 }
             }
         }
@@ -16,20 +18,21 @@ class Solution {
         return count;
     }
 
-    public void dfs(char[][] grid, int r, int c, boolean[][] vis){
-        vis[r][c]=true;
+    public void dfs(char[][] grid, boolean[][] vis, int row, int col){
 
-        int[] row = {0,1,0,-1};
-        int[] col = {1,0,-1,0};
+        vis[row][col] = true;
 
-        for(int i=0;i<4;i++){
-            if(isValid(grid,vis,r+row[i],c+col[i])){
-                dfs(grid,r+row[i],c+col[i],vis);
+        int[] r = {0,1,0,-1};
+        int[] c = {1,0,-1,0};
+
+        for(int i=0; i<4; i++){
+            if(isValid(grid,vis,row+r[i],col+c[i])){
+                dfs(grid,vis,row+r[i],col+c[i]);
             }
         }
     }
 
-    public boolean isValid(char[][] grid, boolean[][] vis, int r, int c){
-        return r>=0 && c>=0 && r<grid.length && c<grid[0].length && !vis[r][c] && grid[r][c]=='1';
+    public boolean isValid(char[][] grid, boolean[][] vis, int row, int col){
+        return row>=0 && row<grid.length && col>=0 && col<grid[0].length && !vis[row][col] && grid[row][col]=='1';
     }
 }
