8a5bd48fa9bf5a37f88b5e665d80c164239c92b3
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-19 15:49:15 -0500

    File updated at 19/04/2025, 15:49:15

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,22 +1,20 @@
+// Last updated: 19/04/2025, 15:49:15
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount==0){
-            return amount;
-        }
-
         int[] dp = new int[amount+1];
 
         Arrays.fill(dp,Integer.MAX_VALUE);
+
         dp[0]=0;
 
         for(int i=1;i<dp.length;i++){
-            for(int j : coins){
-                if(i>=j && dp[i-j]<Integer.MAX_VALUE){
-                    dp[i]=Math.min(dp[i],dp[i-j]+1);
+            for(int coin : coins){
+                if(i>=coin && dp[i-coin] != Integer.MAX_VALUE){
+                    dp[i]= Math.min(1+dp[i-coin],dp[i]);
                 }
             }
         }
 
-        return dp[amount]!=Integer.MAX_VALUE?dp[amount]:-1;
+        return dp[amount]==Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
