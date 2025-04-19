dd22cd69376892e82bc3decf07dc4c97e62a06fe
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-19 13:21:18 -0500

    File updated at 19/04/2025, 13:21:18

--- 91-DecodeWays/91-DecodeWays.java
+++ 91-DecodeWays/91-DecodeWays.java
@@ -1,28 +1,33 @@
+// Last updated: 19/04/2025, 13:21:18
 class Solution {
     public int numDecodings(String s) {
         int[] dp = new int[s.length()+1];
+
+        dp[0]=1;
 
         if(s.charAt(0)=='0'){
             return 0;
         }
 
-        dp[0]=1;
-
         dp[1]=1;
 
-        for(int i=2;i<dp.length;i++){
-            if(s.charAt(i-1)=='0'){
-                if((s.charAt(i-2)!='1' && s.charAt(i-2)!='2')){
-                    return 0;
-                }
+        for(int i=1;i<s.length();i++){
+            char cur = s.charAt(i);
+            char prev = s.charAt(i-1);
 
-                dp[i]=dp[i-2];
+            if(cur=='0' && prev!='1' && prev!='2'){
+                return 0;
             }
-            else if((s.charAt(i-2)=='1') || (s.charAt(i-2)=='2' && ('1'<=s.charAt(i-1) && s.charAt(i-1)<='6'))){
-                dp[i]=dp[i-1]+dp[i-2];
+
+            if(cur!='0'){
+                dp[i+1]=dp[i];
             }
-            else{
-                dp[i]=dp[i-1];
+
+            if(prev=='1'){
+                dp[i+1] +=dp[i-1];
+            }
+            else if(prev=='2' && (cur>='0' && cur<='6')){
+                dp[i+1] +=dp[i-1];
             }
         }
 
