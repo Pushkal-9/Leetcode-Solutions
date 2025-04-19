583b4614d98612f4b3863386db30cb8cb9abd863
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-19 18:01:02 -0500

    File updated at 19/04/2025, 18:01:01

--- 779-K-thSymbolinGrammar/779-K-thSymbolinGrammar.java
+++ 779-K-thSymbolinGrammar/779-K-thSymbolinGrammar.java
@@ -1,14 +1,18 @@
-// Last updated: 19/04/2025, 01:37:52
+// Last updated: 19/04/2025, 18:01:01
 class Solution {
     public int kthGrammar(int n, int k) {
-        if (n == 1) return 0;
-
-        int mid = 1 << (n - 2);
-
-        if (k <= mid) {
-            return kthGrammar(n - 1, k);
-        } else {
-            return 1 - kthGrammar(n - 1, k - mid);
+        if(n==1){
+            return 0;
         }
+
+        int total = (int) Math.pow(2,n-1);
+
+        int mid = total/2;
+
+        if(k<=mid){
+            return kthGrammar(n-1,k);
+        }
+
+        return 1 - kthGrammar(n-1,k-mid);
     }
 }
