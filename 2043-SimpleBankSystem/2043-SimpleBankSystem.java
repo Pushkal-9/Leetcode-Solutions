72518702d5838dae00516111efab3fc264896b59
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-18 22:35:10 -0500

    File updated at 18/04/2025, 22:35:09

--- 2043-SimpleBankSystem/2043-SimpleBankSystem.java
+++ 2043-SimpleBankSystem/2043-SimpleBankSystem.java
@@ -1,64 +1,39 @@
-// Last updated: 18/04/2025, 22:34:07
+// Last updated: 18/04/2025, 22:35:09
+import java.util.Arrays;
+
 class Bank {
+    private final long[] accounts;
 
-    long[] accounts;
-    int numberOfAccounts;
     public Bank(long[] balance) {
-
-        numberOfAccounts = balance.length;
-        accounts = new long[numberOfAccounts];
-
-
-        for(int i=0;i<numberOfAccounts;i++){
-            accounts[i]=balance[i];
-        }
-
+        
+        this.accounts = Arrays.copyOf(balance, balance.length);
     }
-    
+
+    private boolean isValidAccount(int account) {
+        return account >= 1 && account <= accounts.length;
+    }
+
     public boolean transfer(int account1, int account2, long money) {
-        if(numberOfAccounts<account1 || numberOfAccounts<account2){
-            return false;
-        }
+        if (!isValidAccount(account1) || !isValidAccount(account2)) return false;
+        if (accounts[account1 - 1] < money) return false;
 
-        if(accounts[account1-1]<money){
-            return false;
-        }
-
-        accounts[account1-1] = accounts[account1-1]-money;
-        accounts[account2-1] = accounts[account2-1]+money;
-
+        accounts[account1 - 1] -= money;
+        accounts[account2 - 1] += money;
         return true;
     }
-    
+
     public boolean deposit(int account, long money) {
-        if(numberOfAccounts<account){
-            return false;
-        }   
+        if (!isValidAccount(account)) return false;
 
-        accounts[account-1] = accounts[account-1] + money;    
-
+        accounts[account - 1] += money;
         return true;
     }
-    
+
     public boolean withdraw(int account, long money) {
-        if(numberOfAccounts<account){
-            return false;
-        }
+        if (!isValidAccount(account)) return false;
+        if (accounts[account - 1] < money) return false;
 
-        if(accounts[account-1]<money){
-            return false;
-        }    
-
-        accounts[account-1] = accounts[account-1]-money;
-
+        accounts[account - 1] -= money;
         return true;
     }
 }
-
-/**
- * Your Bank object will be instantiated and called as such:
- * Bank obj = new Bank(balance);
- * boolean param_1 = obj.transfer(account1,account2,money);
- * boolean param_2 = obj.deposit(account,money);
- * boolean param_3 = obj.withdraw(account,money);
- */
