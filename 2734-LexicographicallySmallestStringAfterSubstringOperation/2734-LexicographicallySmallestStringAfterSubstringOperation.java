81c818076ccd4558e6c33db45cf8b984ca947a52
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-19 01:19:16 -0500

    File updated at 19/04/2025, 01:19:16

--- 2734-LexicographicallySmallestStringAfterSubstringOperation/2734-LexicographicallySmallestStringAfterSubstringOperation.java
+++ 2734-LexicographicallySmallestStringAfterSubstringOperation/2734-LexicographicallySmallestStringAfterSubstringOperation.java
@@ -1,47 +1,30 @@
-// Last updated: 19/04/2025, 01:18:14
+// Last updated: 19/04/2025, 01:19:16
 class Solution {
     public String smallestString(String s) {
-        int start = -1;
+        char[] chars = s.toCharArray();
+        int i = 0;
 
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!='a'){
-                start=i;
-                break;
-            }
+        // Skip leading 'a's
+        while (i < chars.length && chars[i] == 'a') {
+            i++;
         }
 
-        if(start==-1){
-            return s.substring(0,s.length()-1) + "z";
+        // If all are 'a', change last char to 'z'
+        if (i == chars.length) {
+            chars[chars.length - 1] = 'z';
+            return new String(chars);
         }
 
-        int end = -1;
-
-        for(int i=start;i<s.length();i++){
-
-            if(s.charAt(i)!='a'){
-                end = i;
-            }
-            else{
-                break;
-            }
+        // Start decrementing until next 'a' or end
+        while (i < chars.length && chars[i] != 'a') {
+            chars[i] = getPreviousChar(chars[i]);
+            i++;
         }
 
-        return s.substring(0,start) + buildSubString(start,end,s) + s.substring(end+1,s.length());
+        return new String(chars);
     }
 
-    public String buildSubString(int start, int end, String s){
-        String sub = s.substring(start,end+1);
-
-        StringBuilder sb = new StringBuilder();
-
-        for(char ch : sub.toCharArray()){
-            sb.append(getPreviousChar(ch));
-        }
-
-        return sb.toString();
-    }
-
-    public char getPreviousChar(char ch){
-        return ch == 'a' ? 'z' : (char)(ch - 1);
+    private char getPreviousChar(char ch) {
+        return ch == 'a' ? 'z' : (char) (ch - 1);
     }
 }
