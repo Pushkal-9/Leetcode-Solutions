253307e4366922a940d48cf977a0c416336ad4f9
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-19 21:51:08 -0500

    File updated at 19/04/2025, 21:51:08

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.java
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -1,52 +1,51 @@
-// Last updated: 19/04/2025, 21:50:18
+// Last updated: 19/04/2025, 21:51:08
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int row = findRow(matrix, target);
 
-        int row = findRow(matrix,target);
-
-        int low = 0;
-        int high = matrix[0].length-1;
+        int low = 0, high = matrix[0].length-1;
 
         if (row < 0 || row >= matrix.length || matrix[row][0] > target) {
             return false;
         }
 
-        //System.out.println(row);
+        while(low <= high){
 
+            int mid = (high - low)/2 + low;
 
-        while(low<=high){
-            int mid = (high-low)/2 + low;
-
-            if(matrix[row][mid]==target){
+            if (matrix[row][mid] == target){
                 return true;
             }
-            else if(matrix[row][mid]>target){
-                high = mid-1;
+            else if (matrix[row][mid]<target){
+                low = mid + 1;
             }
             else{
-                low = mid+1;
+                high = mid - 1;
             }
+
         }
-        
+
         return false;
     }
 
     public int findRow(int[][] matrix, int target){
-        int low = 0;
-        int high = matrix.length-1;
 
-        while(low<=high){
-            int mid = (high-low)/2 + low;
+        int low = 0, high = matrix.length-1;
 
-            if(matrix[mid][0]==target){
+        while(low <= high){
+
+            int mid = (high - low)/2 + low;
+
+            if (matrix[mid][0] == target){
                 return mid;
             }
-            else if(matrix[mid][0]>target){
-                high = mid-1;
+            else if (matrix[mid][0]<target){
+                low = mid + 1;
             }
             else{
-                low = mid+1;
+                high = mid - 1;
             }
+
         }
 
         return high;
