1bdf46c14c56c0efcf852b73844349e05dc55797
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-20 18:12:31 -0500

    File updated at 20/04/2025, 18:12:30

--- 135-Candy/135-Candy.java
+++ 135-Candy/135-Candy.java
@@ -1,30 +1,31 @@
-// Last updated: 20/04/2025, 18:06:07
+// Last updated: 20/04/2025, 18:12:30
 class Solution {
     public int candy(int[] ratings) {
-        int min = Integer.MAX_VALUE;
         int n = ratings.length;
-        int[] res = new int[n];
-        
-        Arrays.fill(res,1);
 
-        for(int i=0;i<n-1;i++){
-            if(ratings[i]<ratings[i+1]){
-                res[i+1]=res[i]+1;
+        int[] candy = new int[n];
+
+        Arrays.fill(candy,1);
+
+        for(int i=1 ;i<n; i++){
+            if(ratings[i-1] < ratings[i]){
+
+                candy[i] = candy[i-1] + 1;
+
             }
         }
 
-        for(int i=n-1;i>0;i--){
-            if(ratings[i]<ratings[i-1]){
-                res[i-1]=Math.max(res[i-1],res[i]+1);
+        for(int i = n-2; i>=0; i--){
+
+            if(ratings[i+1] < ratings[i]){
+
+                candy[i] = Math.max(candy[i+1]+1, candy[i]);
+
             }
         }
 
-        int ans = 0;
+        int total = Arrays.stream(candy).sum();
 
-        for(int i=0;i<n;i++){
-            ans=ans+res[i];
-            }
-
-            return ans;
+        return total;
     }
-} 
+}
