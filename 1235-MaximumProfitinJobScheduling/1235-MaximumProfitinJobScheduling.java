d66a40804fe0fe0432de4df07a8f5b4eb5b56fb9
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-20 17:03:54 -0500

    File updated at 20/04/2025, 17:03:54

--- 1235-MaximumProfitinJobScheduling/1235-MaximumProfitinJobScheduling.java
+++ 1235-MaximumProfitinJobScheduling/1235-MaximumProfitinJobScheduling.java
@@ -1,45 +1,51 @@
-// Last updated: 20/04/2025, 16:53:29
+// Last updated: 20/04/2025, 17:03:54
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = startTime.length;
-        Interval[] intervals = new Interval[n];
+        Interval[] intervals = new Interval[startTime.length];
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<intervals.length; i++){
             intervals[i] = new Interval(startTime[i], endTime[i], profit[i]);
         }
 
-        Arrays.sort(intervals, (a, b) -> a.end - b.end);
+        Arrays.sort(intervals, (a,b) -> a.end-b.end);
+
+        int maxProfit = 0;
+
+        int[] mem = new int[profit.length];
+
+        mem[0] = intervals[0].profit;
 
 
-        int[] dp = new int[n];
-        dp[0] = intervals[0].profit;
+        for(int i=1;i<intervals.length;i++){
+            int prevOptimalIndex = findPreviousOptimalIndex(intervals, i);
+            int current = intervals[i].profit;
 
-        for (int i = 1; i < n; i++) {
-            int currProfit = intervals[i].profit;
+            if(prevOptimalIndex != -1)
+                current = mem[prevOptimalIndex] + current;
 
-
-            int lastIndex = findLastNonConflictingJob(intervals, i);
-            if (lastIndex != -1) {
-                currProfit += dp[lastIndex];
-            }
-
-            dp[i] = Math.max(dp[i - 1], currProfit);
+            mem[i] = Math.max(current, mem[i-1]);
         }
 
-        return dp[n - 1];
+        return mem[intervals.length-1];
     }
 
-    private int findLastNonConflictingJob(Interval[] intervals, int index) {
-        int low = 0, high = index - 1;
+
+    public int findPreviousOptimalIndex(Interval[] intervals, int index){
+        int low = 0;
+        int high = index-1;
+
         int result = -1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (intervals[mid].end <= intervals[index].start) {
+        while(low<=high){
+
+            int mid = (high-low)/2 + low;
+
+            if(intervals[mid].end <= intervals[index].start){
                 result = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+                low = mid+1;
+            }
+            else{
+                high = mid -1;
             }
         }
 
@@ -47,14 +53,14 @@ class Solution {
     }
 }
 
-class Interval {
+class Interval{
     int start;
     int end;
     int profit;
 
-    public Interval(int start, int end, int profit) {
-        this.start = start;
-        this.end = end;
-        this.profit = profit;
+    public Interval(int start, int end, int profit){
+        this.start=start;
+        this.end=end;
+        this.profit=profit;
     }
 }
