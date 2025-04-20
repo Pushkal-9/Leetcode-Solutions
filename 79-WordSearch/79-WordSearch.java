fc72df14dd5d2db1276d8485d418029eae80c935
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-19 21:31:00 -0500

    File updated at 19/04/2025, 21:30:59

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,12 +1,22 @@
+// Last updated: 19/04/2025, 21:30:59
 class Solution {
     public boolean exist(char[][] board, String word) {
+        char[] chars = word.toCharArray();
+
         boolean[][] vis;
 
         for(int i=0;i<board.length;i++){
+
             for(int j=0;j<board[0].length;j++){
+
                 vis = new boolean[board.length][board[0].length];
-                if(board[i][j]==word.charAt(0) && exist(board,word,String.valueOf(board[i][j]),i,j,vis)){
-                    return true;
+
+                if(board[i][j]==chars[0]){
+
+                    if(dfs(board, chars, 0, i, j, vis)){
+
+                        return true;
+                    }
                 }
             }
         }
@@ -14,39 +24,36 @@ class Solution {
         return false;
     }
 
-    public boolean exist(char[][] board, String word, String current, int row, int col, boolean[][] vis){
-        if(row<0 || col<0 || row>=board.length || col>=board[0].length){
-            return false;
-        }
+    public boolean dfs(char[][] board, char[] word, int cur, int row, int col, boolean[][] vis){
 
-        if(word.equals(current)){
+        if(cur==word.length-1){
             return true;
         }
 
-        //System.out.println(current);
+        vis[row][col] = true;
 
-        int[] r = {0,0,1,-1};
-        int[] c = {1,-1,0,0};
+        int[] r = {0,1,0,-1};
 
-        boolean res = false;
+        int[] c = {1,0,-1,0};
 
         for(int i=0;i<4;i++){
-            char ch = word.charAt(current.length());
-            if(isValid(board,word,row+r[i],col+c[i],ch,vis)){
-                vis[row][col]=true;
-                String base = current + String.valueOf(ch);
-                //System.out.println(ch);
-                if(exist(board,word,base,row+r[i],col+c[i],vis)){
+
+            if(isSafe(board, vis, row+r[i], col+c[i], word[cur+1])){
+
+                vis[row+r[i]][col+c[i]] = true;
+
+                if(dfs(board, word, cur+1, row+r[i], col+c[i], vis)){
+
                     return true;
                 }
-                vis[row][col]=false;
+                vis[row+r[i]][col+c[i]] = false;
             }
         }
 
         return false;
     }
 
-    public boolean isValid(char[][] board, String word, int row, int col, char ch, boolean[][] vis){
+    public boolean isSafe(char[][] board, boolean[][] vis, int row, int col, char ch){
         return row>=0 && col>=0 && row<board.length && col<board[0].length && !vis[row][col] && ch==board[row][col];
     }
 }
