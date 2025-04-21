a616625c209b8352d4de9c0fdf51378ef0f2075c
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-21 00:36:37 -0500

    Create file

---
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -0,0 +1,38 @@
+// Last updated: 21/04/2025, 00:36:36
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int first = nums1.length;
+        int second = nums2.length;
+
+        int total = first + second;
+
+        int half = total/2;
+
+        int left = 0;
+        int right = nums1.length;
+
+        while(true){
+            int mid = (left+right)/2;
+            int rem = half - mid;
+
+            int firstLeft = mid>0 ? nums1[mid-1] : Integer.MIN_VALUE;
+            int secondLeft = rem>0 ? nums2[rem-1] : Integer.MIN_VALUE;
+            int secondRight = rem<second ? nums2[rem] : Integer.MAX_VALUE;
+            int firstRight = mid<first ? nums1[mid] : Integer.MAX_VALUE;
+
+            if(firstLeft<=secondRight && secondLeft<=firstRight){
+                if(total%2==0){
+                    return (double)(Math.max(firstLeft,secondLeft) + Math.min(secondRight,firstRight))/(double)2;
+                }
+                return (double)Math.min(firstRight,secondRight);
+            }
+            else if(firstLeft>secondRight){
+                right = right-1;
+            }
+            else{
+                left = left+1;
+            }
+
+        }
+    }
+}
