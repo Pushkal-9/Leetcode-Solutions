3c3d93cc3f03f776b683871cd529337e363e59eb
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-20 20:19:11 -0500

    File updated at 20/04/2025, 20:19:11

--- 1838-FrequencyoftheMostFrequentElement/1838-FrequencyoftheMostFrequentElement.java
+++ 1838-FrequencyoftheMostFrequentElement/1838-FrequencyoftheMostFrequentElement.java
@@ -1,22 +1,25 @@
-// Last updated: 20/04/2025, 20:16:55
+// Last updated: 20/04/2025, 20:19:11
 class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
 
         int left = 0;
+
         long curr = 0;
+
         int max = 0;
 
-        for(int right = 0; right < nums.length; right++){
-            curr += nums[right];
+        for(int right = 0; right<nums.length; right++){
 
-            // Total cost to make all elements in the window equal to nums[right]
-            while((long)(right - left + 1) * nums[right] - curr > k){
-                curr -= nums[left];
+            int target = nums[right];
+            curr = curr + target;
+
+            while( (long)(right-left+1) * target - curr > k){
+                curr = curr - nums[left];
                 left++;
             }
 
-            max = Math.max(max, right - left + 1);
+            max = Math.max(max,right-left+1);
         }
 
         return max;
