77fef5c96c9b1dc38548cef1753c359dc31b81da
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-04-26 14:35:43 -0500

    File updated at 26/04/2025, 14:35:43

--- 752-OpentheLock/752-OpentheLock.java
+++ 752-OpentheLock/752-OpentheLock.java
@@ -1,47 +1,71 @@
-// Last updated: 26/04/2025, 14:35:29
+// Last updated: 26/04/2025, 14:35:43
 class Solution {
     public int openLock(String[] deadends, String target) {
-        HashSet<String> visited = new HashSet<>();
-
-        HashSet<String> dead = new HashSet<>(Arrays.asList(deadends));
+        HashSet<String> dead = new HashSet<>();
+        HashSet<String> vis = new HashSet<>();
+        for(String deadend : deadends){
+            dead.add(deadend);
+        }
 
         if(dead.contains("0000")){
             return -1;
         }
 
-        visited.add("0000");
-
         Queue<Pair<String,Integer>> q = new LinkedList<>();
 
         q.add(new Pair<>("0000",0));
+        vis.add("0000");
+
+        int[] steps = {-1,1};
+
 
         while(!q.isEmpty()){
-            Pair<String,Integer> p = q.poll();
+            Pair<String, Integer> p = q.poll();
+
             String key = p.getKey();
-            int val = p.getValue();
+            int dist = p.getValue();
 
             if(key.equals(target)){
-                return val;
+                return dist;
             }
 
-
             for(int i=0;i<4;i++){
-                char ch = key.charAt(i);
-                for(int j : new int[]{1, -1}){
-                    char newChar = (char)((((ch - '0') + j + 10) % 10) + '0');
-                    StringBuilder sb = new StringBuilder(key);
-                    sb.setCharAt(i,newChar);
-                    String newPath = sb.toString();
+                for(int step : steps){
+                    String comb = nextCombination(key,i,step);
 
-                    if(!dead.contains(newPath) && !visited.contains(newPath)){
-                        Pair<String,Integer> newPair = new Pair<>(newPath, val+1);
-                        visited.add(newPath);
-                        q.add(newPair);
+                    if(!dead.contains(comb) && !vis.contains(comb)){
+                        Pair<String,Integer> pair = new Pair<>(comb, dist+1);
+                        q.add(pair);
+                        vis.add(comb);
                     }
                 }
             }
         }
 
         return -1;
+    }
+
+    public String nextCombination(String comb, int pos, int step){
+        char ch = comb.charAt(pos);
+
+        char mod;
+
+        if(step==-1 && ch=='0'){
+            mod = '9';
+        }
+        else if(step==1 && ch=='9'){
+            mod = '0';
+        }
+        else{
+            mod = (char)(ch + step);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(comb.substring(0,pos));
+        sb.append(mod);
+        sb.append(comb.substring(pos+1,comb.length()));
+
+        return sb.toString();
     }
 }
