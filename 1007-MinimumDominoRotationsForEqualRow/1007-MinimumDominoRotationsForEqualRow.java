a5fcc8cd300eaf9676c78b7fd8f452b5a040fb50
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-05-03 18:21:55 -0400

    File updated at 03/05/2025, 18:21:55

--- 1007-MinimumDominoRotationsForEqualRow/1007-MinimumDominoRotationsForEqualRow.java
+++ 1007-MinimumDominoRotationsForEqualRow/1007-MinimumDominoRotationsForEqualRow.java
@@ -1,4 +1,4 @@
-// Last updated: 03/05/2025, 18:20:41
+// Last updated: 03/05/2025, 18:21:55
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
 
@@ -9,6 +9,7 @@ class Solution {
             int current = i;
             int count = 0;
             int topCount = 0;
+            int bottomCount = 0;
             for(int j=0;j<tops.length;j++){
                 if(tops[j]==current || bottoms[j]==current){
                     count++;
@@ -17,29 +18,13 @@ class Solution {
                 if(tops[j]==current){
                     topCount++;
                 }
-            }
-
-            if(count == tops.length){
-                min = Math.min(min, Math.min(topCount, tops.length-topCount));
-                pos = true;
-            }
-        }
-
-        for(int i=1;i<=6;i++){
-            int current = i;
-            int count = 0;
-            int bottomCount = 0;
-            for(int j=0;j<tops.length;j++){
-                if(bottoms[j]==current || tops[j]==current){
-                    count++;
-                }
-
                 if(bottoms[j]==current){
                     bottomCount++;
                 }
             }
 
             if(count == tops.length){
+                min = Math.min(min, Math.min(topCount, tops.length-topCount));
                 min = Math.min(min, Math.min(bottomCount, tops.length-bottomCount));
                 pos = true;
             }
