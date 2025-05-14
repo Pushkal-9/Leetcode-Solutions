0cb829e2279c9c3074e1a8016312d7c4f37f9545
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-05-14 16:46:26 -0400

    File updated at 14/05/2025, 15:46:26

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,60 +1,60 @@
+// Last updated: 14/05/2025, 15:46:26
 class Solution {
-    HashMap<Integer, ArrayList<Integer>> adj = new HashMap<>();
+    HashMap<Integer, List<Integer>> adj;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean[] vis = new boolean[numCourses];
-
-        for(int[] pre : prerequisites){
-            int node = pre[0];
-            int neighbor = pre[1];
-
-            adj.putIfAbsent(node,new ArrayList<>());
-            adj.get(node).add(neighbor);
-        }
+        buildAdj(numCourses, prerequisites);
 
         int[] indegrees = new int[numCourses];
 
-        for(int i=0;i<numCourses;i++){
-            if(adj.containsKey(i)){
-                for(int to : adj.get(i)){
-                    indegrees[to]++;
-                }
-            }
+        for(int i=0;i<prerequisites.length;i++){
+            indegrees[prerequisites[i][0]]++;
         }
 
         Queue<Integer> q = new LinkedList<>();
 
-        for(int i=0;i<numCourses;i++){
+        boolean[] vis = new boolean[numCourses];
+
+        for(int i=0;i<indegrees.length;i++){
             if(indegrees[i]==0){
                 q.add(i);
+                vis[i]=true;
             }
         }
 
-        int[] res = new int[numCourses];
-
-        int index=0;
-
         while(!q.isEmpty()){
             int node = q.poll();
-            res[index]=node;
-            index++;
 
-            if(!adj.containsKey(node)){
-                continue;
-            }
-            for(int in : adj.get(node)){
-                indegrees[in]--;
-
-                if(indegrees[in]==0){
-                    q.add(in);
+            for(int cur : adj.get(node)){
+                if(!vis[cur]){
+                    indegrees[cur]--;
+                    
+                    if(indegrees[cur]==0){
+                        q.add(cur);
+                        vis[cur]=true;
+                    }
                 }
             }
         }
 
-        if(index!=numCourses){
-            return false;
+        for(boolean node : vis){
+            if(!node){
+                return false;
+            }
         }
 
         return true;
+    }
 
+    public void buildAdj(int n, int[][] pre){
+
+        adj = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            adj.put(i, new ArrayList<>());
+        }
+
+        for(int[] pair : pre){
+            adj.get(pair[1]).add(pair[0]);
+        }
     }
 }
