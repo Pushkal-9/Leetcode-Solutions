8d08a25fc4dd08c8d6cbb11a6a2c17884fa0f28c
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-05-15 18:49:31 -0400

    File updated at 15/05/2025, 17:49:31

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,14 +1,15 @@
+// Last updated: 15/05/2025, 17:49:31
 class Solution {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount+1];
 
         dp[0]=1;
 
+
         for(int coin : coins){
             for(int i=1;i<=amount;i++){
-                int current = i;
-                if(current-coin>=0 && dp[current-coin]!=0){
-                    dp[i]=dp[i]+dp[current-coin];
+                if(i>=coin && dp[i-coin]!=0){
+                    dp[i]=dp[i-coin]+dp[i];
                 }
             }
         }
