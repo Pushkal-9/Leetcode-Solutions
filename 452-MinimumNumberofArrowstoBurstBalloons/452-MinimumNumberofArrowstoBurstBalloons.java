1b6e98e94a4458c542eaa9fff85fb93894c05627
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-05-16 00:08:57 -0400

    File updated at 15/05/2025, 23:08:57

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.java
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.java
@@ -1,16 +1,14 @@
-// Last updated: 19/04/2025, 16:17:07
+// Last updated: 15/05/2025, 23:08:57
 class Solution {
     public int findMinArrowShots(int[][] points) {
-
-        Arrays.sort(points, (a,b)-> {
-            if(a[1] == b[1]){
+        Arrays.sort(points, (a,b) -> {
+            if(a[1]==b[1]){
                 return 0;
             }
 
             if(a[1]>b[1]){
                 return 1;
             }
-
             return -1;
         });
 
@@ -18,8 +16,8 @@ class Solution {
 
         int count = 1;
 
-        for(int i=1;i<points.length;i++){
-            if(points[i][0]<=end){
+        for(int i=0;i<points.length;i++){
+            if(end>=points[i][0]){
                 continue;
             }
             else{
@@ -29,6 +27,5 @@ class Solution {
         }
 
         return count;
-
     }
 }
