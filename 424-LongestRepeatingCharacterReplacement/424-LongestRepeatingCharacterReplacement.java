8b42a2bc47c577f46a2bc65531b94dfc84569cbb
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-05-16 00:01:29 -0400

    File updated at 15/05/2025, 23:01:29

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
@@ -1,30 +1,34 @@
-// Last updated: 19/04/2025, 15:43:05
+// Last updated: 15/05/2025, 23:01:29
 class Solution {
     public int characterReplacement(String s, int k) {
-        int n = s.length();
-
         int max = 0;
 
         for(int i=0;i<26;i++){
             char ch = (char)('A' + i);
-            int end=0, start=0;
-            int count=0;
+
+            int start=0;
+            int end = 0;
+
+            int count = 0;
 
             while(end<s.length()){
-                if(s.charAt(end)!=ch){
-                    count++;
+                if(s.charAt(end)==ch){
+                    max = Math.max(end-start+1,max);
+                    end++;
                 }
-
-                while(count>k){
-                    if(s.charAt(start)!=ch){
-                        count--;
+                else{
+                    if(count<k){
+                        count++;
+                        max = Math.max(end-start+1, max);
+                        end++;       
                     }
-                    start++;
+                    else{
+                        if(s.charAt(start)!=ch){
+                            count--;
+                        }
+                        start++;
+                    }
                 }
-
-                max=Math.max(end-start+1,max);
-
-                end++;
             }
         }
 
