2569192722297fe826ad67eb3c9ab7ea199984f5
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-05-24 23:22:27 -0500

    File updated at 24/05/2025, 23:22:27

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,13 +1,13 @@
-// Last updated: 16/05/2025, 00:25:38
+// Last updated: 24/05/2025, 23:22:27
 class Solution {
-    boolean[][] vis ;
     public boolean exist(char[][] board, String word) {
-        
+        boolean[][] vis;
+
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 vis = new boolean[board.length][board[0].length];
                 if(board[i][j]==word.charAt(0)){
-                    if(exist(board,word,i,j,"")){
+                    if(exist(board,word,vis,i,j,0)){
                         return true;
                     }
                 }
@@ -17,32 +17,29 @@ class Solution {
         return false;
     }
 
-    public boolean exist(char[][] board, String word, int row, int col, String current){
-        if(current.equals(word)){
+    public boolean exist(char[][] board, String word, 
+                                                boolean[][] vis, int row, int col, int index){
+        if(index>=word.length()){
             return true;
         }
 
-        int[] r = {0,1,0,-1};
-        int[] c = {1,0,-1,0};
+        int[] r = {0,0,-1,1};
+        int[] c = {1,-1,0,0};
 
-        boolean res = false;
-
-        if(isSafe(board,row,col,current,word)){
-            vis[row][col]=true;
+        if(isSafe(board,word,row,col,vis,index)){
+            vis[row][col] = true;
             for(int i=0;i<4;i++){
-                String newCurrent = current + String.valueOf(board[row][col]);
-                if(exist(board,word,row+r[i],col+c[i],newCurrent)){
+                if(exist(board,word,vis,row+r[i],col+c[i],index+1)){
                     return true;
                 }
             }
-            vis[row][col]=false;
+            vis[row][col] = false;
         }
 
         return false;
     }
 
-    public boolean isSafe(char[][] board, int row, int col, String current, String word){
-        return row>=0 && col>=0 && row<board.length && col<board[0].length && word.length()>current.length() &&
-                board[row][col]==word.charAt(current.length()) && !vis[row][col];
+    public boolean isSafe(char[][] board, String word, int row, int col, boolean[][] vis, int index){
+        return row>=0 && col>=0 && row<board.length && col<board[0].length && !vis[row][col] && board[row][col]==word.charAt(index);
     }
 }
