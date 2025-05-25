b07dcbcacb416dbcc19cfda9db014309e78720e7
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-05-24 23:33:28 -0500

    File updated at 24/05/2025, 23:33:28

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,28 +1,33 @@
-// Last updated: 15/05/2025, 20:09:22
+// Last updated: 24/05/2025, 23:33:28
 class Solution {
     public String longestPalindrome(String s) {
-        boolean[][] dp = new boolean[s.length()][s.length()];
+        int n = s.length();
+        int[][] dp = new int[n][n];
 
-        for(int i=0;i<dp.length;i++){
-            dp[i][i] = true;
+        for(int i=0;i<n;i++){
+            dp[i][i]=1;
         }
 
-        String max =  s.substring(0,1);
+        int max = 1;
 
-        for(int i=1;i<s.length();i++){
-            for(int j=0;i+j<s.length();j++){
-                if(s.charAt(j)==s.charAt(i+j)){
-                    if(i==1 || dp[j+1][i+j-1]){
-                        dp[j][i+j]=true;
-                    }
+        String res = s.substring(0,1);
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j+i<n;j++){
+                if(i==1 && s.charAt(j)==s.charAt(j+i)){
+                    dp[j][j+i] = 2;
+                }
+                else if(s.charAt(j)==s.charAt(j+i) && dp[j+1][j+i-1]!=0){
+                    dp[j][j+i] = 2 + dp[j+1][j+i-1];
                 }
 
-                if(dp[j][i+j]){
-                    max = s.substring(j,i+j+1);
+                if(max<dp[j][j+i]){
+                    max = Math.max(max,dp[j][j+i]);
+                    res = s.substring(j,j+i+1);
                 }
             }
         }
 
-        return max;
+        return res;
     }
 }
