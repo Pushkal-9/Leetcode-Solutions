5023a6d2251a5dea8c52539ff847672a8032c6a9
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-16 16:44:29 -0500

    File updated at 16/06/2025, 16:44:29

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,14 +1,14 @@
-// Last updated: 15/05/2025, 16:45:51
+// Last updated: 16/06/2025, 16:44:29
 class Solution {
     public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length - 1;
+
         int max = 0;
 
-        int start = 0, end = height.length-1;
-
         while(start<=end){
-            
-            max = Math.max(max, Math.min(height[start],height[end]) * (end-start));
 
+            max = Math.max(max, Math.min(height[start],height[end]) *(end-start));
             if(height[start]<height[end]){
                 start++;
             }
