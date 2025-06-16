df04eca09589920d42332a215a59c13e6fe83b45
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-16 17:06:44 -0500

    File updated at 16/06/2025, 17:06:44

--- 1041-RobotBoundedInCircle/1041-RobotBoundedInCircle.java
+++ 1041-RobotBoundedInCircle/1041-RobotBoundedInCircle.java
@@ -1,6 +1,7 @@
-// Last updated: 15/05/2025, 22:51:53
+// Last updated: 16/06/2025, 17:06:44
 class Solution {
     public boolean isRobotBounded(String instructions) {
+        
         int[] dx = {0, -1, 0, 1};
         int[] dy = {1, 0, -1, 0};
 
@@ -9,21 +10,21 @@ class Solution {
 
         int index = 0;
 
-        for(int i=0; i<instructions.length(); i++){
-            char ch = instructions.charAt(i);
+        for(int i=0; i < instructions.length(); i++){
+            char in = instructions.charAt(i);
 
-            if(ch == 'G'){
+            if(in == 'G'){
                 x = x + dx[index];
                 y = y + dy[index];
             }
-            else if(ch == 'L'){
-                index = (index + 1)%4;
+            else if( in == 'L'){
+                index = (index+1)%4;
             }
             else{
-                index = (index + 3)%4;
+                index = (index+3)%4;
             }
         }
 
-        return (x==0 && y==0) || index!=0;
+        return (x==0 && y==0) || (index!=0);
     }
 }
