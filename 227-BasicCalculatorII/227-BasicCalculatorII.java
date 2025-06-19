b57a586f0dec2e7708c9655a95914d5c78b60669
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-19 18:08:22 -0500

    File updated at 19/06/2025, 18:08:22

--- 227-BasicCalculatorII/227-BasicCalculatorII.java
+++ 227-BasicCalculatorII/227-BasicCalculatorII.java
@@ -1,57 +1,98 @@
-// Last updated: 22/04/2025, 19:18:57
+// Last updated: 19/06/2025, 18:08:22
 class Solution {
     public int calculate(String s) {
-        List<String> list = new ArrayList<>();
+        List<String> list = sanatizeExpression(s);
 
-        s = s.trim();
-        int start = 0;
+        List<String> cur = new ArrayList<>();
 
-        while (start < s.length()) {
-            if (s.charAt(start) == ' ') {
-                start++;
+        for(int i=0;i<list.size();i++){
+            String current = list.get(i);
+            if(current.equals("/") || current.equals("*")){
+                String prev = cur.get(cur.size()-1);
+                String next = list.get(i+1);
+
+                Integer first = Integer.parseInt(prev);
+                Integer second = Integer.parseInt(next);
+
+                Integer val;
+
+                if(current.equals("/")){
+                    val = first / second;
+                }
+                else{
+                    val = first * second;
+                }
+
+                cur.remove(cur.size()-1);
+                cur.add(String.valueOf(val));
+                i++;
+            }
+            else{
+                cur.add(String.valueOf(current));
+            }
+        }
+
+        list = new ArrayList<>(cur);
+        cur = new ArrayList<>();
+
+        for(int i=0;i<list.size();i++){
+            String current = list.get(i);
+            if(current.equals("+") || current.equals("-")){
+                String prev = cur.get(cur.size()-1);
+                String next = list.get(i+1);
+
+                Integer first = Integer.parseInt(prev);
+                Integer second = Integer.parseInt(next);
+
+                Integer val;
+
+                if(current.equals("+")){
+                    val = first + second;
+                }
+                else{
+                    val = first - second;
+                }
+
+                cur.remove(cur.size()-1);
+                cur.add(String.valueOf(val));
+                i++;
+            }
+            else{
+                cur.add(String.valueOf(current));
+            }
+        }
+
+        return Integer.parseInt(cur.get(0));
+    }
+
+    public List<String> sanatizeExpression(String s){
+
+        List<String> res = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(ch == ' '){
                 continue;
             }
 
-            if (Character.isDigit(s.charAt(start))) {
-                StringBuilder sb = new StringBuilder();
-                while (start < s.length() && Character.isDigit(s.charAt(start))) {
-                    sb.append(s.charAt(start));
-                    start++;
-                }
-                list.add(sb.toString());
-            } else {
-                list.add(String.valueOf(s.charAt(start)));
-                start++;
+            if(ch == '/' || ch == '*' || ch == '+' || ch == '-'){
+
+                res.add(sb.toString());
+                res.add(String.valueOf(ch));
+                sb = new StringBuilder();
+
+            }
+            else{
+                sb.append(ch);
             }
         }
 
-        List<String> curr = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            String token = list.get(i);
-            if (token.equals("/") || token.equals("*")) {
-                int prev = Integer.parseInt(curr.remove(curr.size() - 1));
-                int next = Integer.parseInt(list.get(++i));
-                int val = token.equals("/") ? prev / next : prev * next;
-                curr.add(String.valueOf(val));
-            } else {
-                curr.add(token);
-            }
+
+        if(!sb.isEmpty()){
+            res.add(sb.toString());
         }
 
-        list = new ArrayList<>(curr);
-        curr = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            String token = list.get(i);
-            if (token.equals("+") || token.equals("-")) {
-                int prev = Integer.parseInt(curr.remove(curr.size() - 1));
-                int next = Integer.parseInt(list.get(++i));
-                int val = token.equals("+") ? prev + next : prev - next;
-                curr.add(String.valueOf(val));
-            } else {
-                curr.add(token);
-            }
-        }
-
-        return Integer.parseInt(curr.get(0));
+        return res;
     }
 }
