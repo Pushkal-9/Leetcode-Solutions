8cb6ed2762ad94374e717fb0798da6046c2bd0fa
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-19 14:38:07 -0500

    File updated at 19/06/2025, 14:38:06

--- 314-BinaryTreeVerticalOrderTraversal/314-BinaryTreeVerticalOrderTraversal.java
+++ 314-BinaryTreeVerticalOrderTraversal/314-BinaryTreeVerticalOrderTraversal.java
@@ -1,4 +1,4 @@
-// Last updated: 28/03/2025, 15:23:45
+// Last updated: 19/06/2025, 14:38:06
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,56 +16,69 @@
  */
 class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        HashMap<Integer, List<Pair>> map = new HashMap<>();
+        Queue<Pair> q = new LinkedList<>();
 
-        vertical(root,0,map,0);
+        Map<Integer,List<Integer>> map = new HashMap<>();
 
-       List<Integer> keySet = new ArrayList<>(map.keySet());
-    Collections.sort(keySet);
+        q.add(new Pair(root,0));
 
-       List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        for(int level : keySet){
-            List<Integer> list = new ArrayList<>();
-            List<Pair> pairList = map.get(level);
-        Collections.sort(pairList,(a,b) -> {
-            if(a.height!=b.height){
-                return a.height-b.height;
-            }
-
-            return 0;
-        });
-            for(Pair p : pairList){
-                list.add(p.value);
-            }
-            res.add(list);
-        } 
-
-        return res;
-    }
-
-    public void vertical(TreeNode root, int level, HashMap<Integer,List<Pair>> map, int height){
-        if(root==null){
-            return;
+        if(root == null){
+            return result;
         }
 
-        if(!map.containsKey(level)){
-            map.put(level,new ArrayList<>());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        while(!q.isEmpty()){
+
+            int size = q.size();
+
+            for(int i=0;i<size;i++){
+
+                Pair pair = q.poll();
+
+                TreeNode node = pair.node;
+                int level = pair.level;
+
+                if(!map.containsKey(level)){
+                    map.put(level, new ArrayList<>());
+                }
+                map.get(level).add(node.val);
+
+                min = Math.min(min,level);
+                max = Math.max(max,level);
+
+
+                if(node.left!=null){
+                    q.add(new Pair(node.left, level-1));
+                }
+
+                if(node.right!=null){
+                    q.add(new Pair(node.right, level+1));
+                }
+
+            }
         }
 
-        map.get(level).add(new Pair(root.val,height));
+        for(int i = min; i<=max;i++){
+            if(map.containsKey(i)){
+                result.add(map.get(i));
+            }
+        }
 
-        vertical(root.left,level-1,map, height+1);
-        vertical(root.right,level+1,map, height+1);
+        return result;
     }
 }
 
 class Pair{
-    int value;
-    int height;
 
-    public Pair(int value, int height){
-        this.value=value;
-        this.height=height;
+    TreeNode node;
+    int level;
+
+    public Pair(TreeNode node, int level){
+        this.node = node;
+        this.level = level;
     }
 }
