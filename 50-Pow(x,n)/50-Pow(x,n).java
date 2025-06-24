29660f235a86246a2b15627c57f5ec886bc86504
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-23 22:33:44 -0500

    File updated at 23/06/2025, 22:33:44

--- 50-Pow(x,n)/50-Pow(x,n).java
+++ 50-Pow(x,n)/50-Pow(x,n).java
@@ -1,29 +1,27 @@
-// Last updated: 23/06/2025, 22:32:46
+// Last updated: 23/06/2025, 22:33:44
 class Solution {
     public double myPow(double x, int n) {
-        if(x==1){
+        if(n == 0 || x == 1){
             return 1;
         }
-        if(n<0){
-            return 1/x * myPow(1/x, -(n + 1));
+
+        if(x == 0){
+            return 0;
         }
-        if(n==0){
-            return 1;
+        if(n < 0){
+            return 1/x * myPow(1/x, -1 * (n+1));
         }
-        if(n==1){
+
+        if(n == 1){
             return x;
         }
 
-        int half = n/2;
+        double subPower = myPow(x,n/2);
 
-        double sub = myPow(x, half);
-
-        double halfPow = sub*sub;
-
-        if(n%2==0){
-            return halfPow;
+        if(n%2 == 0){
+            return subPower * subPower;
         }
 
-        return halfPow * x;
+        return x * subPower * subPower;
     }
 }
