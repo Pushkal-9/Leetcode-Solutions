7ed3a637991fcbd254fbd1ea3f378deb2807bae9
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-25 22:05:31 -0500

    File updated at 25/06/2025, 22:05:31

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,33 +1,34 @@
-// Last updated: 15/05/2025, 23:22:08
+// Last updated: 25/06/2025, 22:05:31
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
 
-        int start = intervals[0][0], end = intervals[0][1];
+        int start = intervals[0][0];
+        int end = intervals[0][1];
 
         List<int[]> list = new ArrayList<>();
 
-        for(int i=0;i<intervals.length;i++){
-            if(end>=intervals[i][0]){
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<=end){
                 end = Math.max(end, intervals[i][1]);
             }
             else{
-                int[] in = {start,end};
-                list.add(in);
+                int[] interval = {start,end};
+                list.add(interval);
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
 
-        int[] in = {start,end};
-        list.add(in);
+        int[] interval = {start,end};
+        list.add(interval);
 
-        int[][] result = new int[list.size()][2];
+        int[][] res = new int[list.size()][2];
 
         for(int i=0;i<list.size();i++){
-            result[i] = list.get(i);
+            res[i] = list.get(i);
         }
 
-        return result;
+        return res;
     }
 }
