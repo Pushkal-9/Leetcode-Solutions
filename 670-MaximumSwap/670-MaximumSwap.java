910792092f6ab28dbd83ac3e895de4a8d04e1e0e
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-28 14:31:58 -0500

    File updated at 28/06/2025, 14:31:58

--- 670-MaximumSwap/670-MaximumSwap.java
+++ 670-MaximumSwap/670-MaximumSwap.java
@@ -1,24 +1,31 @@
-// Last updated: 28/06/2025, 14:22:37
+// Last updated: 28/06/2025, 14:31:58
 class Solution {
     public int maximumSwap(int num) {
-        char[] arr = String.valueOf(num).toCharArray();
         int[] lastIndex = new int[10];
 
-        for (int i = 0; i < arr.length; i++) {
+        char[] arr = String.valueOf(num).toCharArray();
+
+        for(int i=0 ; i<arr.length;i++){
             lastIndex[arr[i] - '0'] = i;
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int d = 9; d > arr[i] - '0'; d--) {
-                if (lastIndex[d] > i) {
-
-                    char temp = arr[i];
-                    arr[i] = arr[lastIndex[d]];
-                    arr[lastIndex[d]] = temp;
-                    return Integer.parseInt(new String(arr));
+        for(int i=0;i<arr.length;i++){
+            for(int d=9;d>=0;d--){
+                int cur = arr[i] -'0';
+                if(cur < d){
+                    if(lastIndex[d] > i){
+                        swap(arr,i,lastIndex[d]);
+                        return Integer.parseInt(String.valueOf(arr));
+                    }
                 }
             }
         }
         return num;
+    }
+
+    public void swap(char[] arr, int i, int j){
+        char ch = arr[i];
+        arr[i] = arr[j];
+        arr[j] = ch;
     }
 }
