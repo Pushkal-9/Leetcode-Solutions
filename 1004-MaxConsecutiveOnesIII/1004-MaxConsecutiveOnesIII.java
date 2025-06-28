1e30ecc31d5870060d339ea0a57a28a2eb255020
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-28 12:57:46 -0500

    File updated at 28/06/2025, 12:57:46

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,30 +1,30 @@
-// Last updated: 18/04/2025, 16:49:05
+// Last updated: 28/06/2025, 12:57:46
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int maxLength=0;
-
         int start = 0;
         int end = 0;
 
-        int current = 0;
+        int max = 0;
 
-        int max=0;
+        int count = 0;
 
         while(end<nums.length){
-            if(nums[end]==1){
-                max=Math.max(max,end-start+1);
-                end++;
-            }
-            else if(current>=k){
-                if(nums[start]==0){
-                    current--;
+            if(nums[end] == 0){
+                if(count < k){
+                    count++;
+                    max = Math.max(end-start+1, max);
+                    end++;
                 }
-                start++;
+                else{
+                    if(nums[start]==0){
+                        count--;
+                    }
+                    start++;
+                }
             }
             else{
-                max=Math.max(max,end-start+1);
+                max = Math.max(end-start+1, max);
                 end++;
-                current++;
             }
         }
 
