22ca086eed928cb7b3be7e82b928668809c87d80
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-28 17:47:38 -0500

    File updated at 28/06/2025, 17:47:38

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.java
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -1,31 +1,32 @@
-// Last updated: 15/05/2025, 18:17:51
+// Last updated: 28/06/2025, 17:47:38
 class Solution {
     public int countSubstrings(String s) {
         boolean[][] dp = new boolean[s.length()][s.length()];
 
-        int count = s.length();
-
-        for(int i=0;i<s.length();i++){
-            dp[i][i]=true;
+        for(int i=0; i<s.length(); i++){
+            dp[i][i] = true;
         }
 
+        int count = s.length();
+
         for(int i=1;i<s.length();i++){
-            for(int j=0;j<s.length()-i;j++){
-                if(s.charAt(j)==s.charAt(j+i)){
-                if(i==1){
-                    dp[j][j+i]=true;
-                }
-                else if (dp[j+1][j+i-1]){
-                    dp[j][j+i]=true;
+            for(int j=0;j+i<s.length();j++){
+                if(s.charAt(j) == s.charAt(j+i)){
+                    if(i==1){
+                        dp[j][j+i] = true;
+                    }
+                    else{
+                        dp[j][j+i] = dp[j+1][j+i-1];
+                    }
                 }
 
                 if(dp[j][j+i]){
                     count++;
                 }
-                }
             }
         }
 
         return count;
+
     }
 }
