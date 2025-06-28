eebd4948cdce3cbddd277df4e62a754669d5a211
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-06-28 18:13:28 -0500

    File updated at 28/06/2025, 18:13:28

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,31 +1,39 @@
+// Last updated: 28/06/2025, 18:13:28
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashSet<List<Integer>> resultSet = new HashSet<>();
-
         Arrays.sort(nums);
 
-        for(int i=0;i<nums.length-2;i++){
-            int j=i+1;
-            int k=nums.length-1;
+        Set<List<Integer>> list = new HashSet<>();
+
+        for(int i=0;i< nums.length;i++){
+            int j = i+1;
+            int k = nums.length-1;
 
             while(j<k){
-                List<Integer> list = new ArrayList<>();
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum==0){
-                    list.addAll(Arrays.asList(nums[i],nums[j],nums[k]));
-                    resultSet.add(list);
+                
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum == 0){
+                    List<Integer> l = new ArrayList<>();
+
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    l.add(nums[k]);
+
+                    list.add(l);
                     j++;
                     k--;
                 }
-                else if(sum<0){
-                    j++;
+                else if( sum>0){
+                    k--;
                 }
                 else{
-                    k--;
+                    j++;
                 }
             }
         }
 
-        return new ArrayList<>(resultSet);
+
+        return new ArrayList<>(list);
     }
 }
