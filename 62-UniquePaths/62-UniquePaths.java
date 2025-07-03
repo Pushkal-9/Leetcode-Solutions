8241b82d17d9135d741282b0ff8423c66cca6f4e
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-07-02 20:27:09 -0500

    File updated at 02/07/2025, 20:27:09

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,13 +1,18 @@
+// Last updated: 02/07/2025, 20:27:09
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m+1][n+1];
 
-        dp[1][1]=1;
+       
 
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(i!=1 || j!=1)
-                    dp[i][j]=dp[i-1][j] + dp[i][j-1];
+                if(i==1 && j==1){
+                     dp[1][1] = 1;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
             }
         }
 
