730bbbd1474bb80fa9852f7f37ad49c3ac9695b1
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2025-07-02 20:48:53 -0500

    File updated at 02/07/2025, 20:48:53

--- 503-NextGreaterElementII/503-NextGreaterElementII.java
+++ 503-NextGreaterElementII/503-NextGreaterElementII.java
@@ -1,29 +1,26 @@
-// Last updated: 19/04/2025, 19:27:38
+// Last updated: 02/07/2025, 20:48:53
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int[] result = new int[nums.length];
+       int[] result = new int[nums.length];
 
-        Arrays.fill(result,-1);
+       Arrays.fill(result, -1);
 
-        Stack<Integer> stack = new Stack<>();
+       Stack<Integer> stack = new Stack<>();
 
-        for(int i=0;i<2*nums.length - 1;i++){
-            
-            int index = i%(nums.length);
-
-            if(stack.isEmpty()){
-                stack.push(index);
-            }
-            else{
-
-                while(!stack.isEmpty() && nums[stack.peek()]<nums[index]){
-                    result[stack.pop()] = nums[index];
-                }
-
-                stack.add(index);
-            }
+       for(int i=0;i<2 * nums.length;i++){
+        int idx = i%nums.length;
+        if(stack.isEmpty()){
+            stack.push(idx);
         }
+        else{
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[idx]){
+                result[stack.pop()] = nums[idx];
+            }
 
-        return result;
+            stack.push(idx);
+        }
+       }
+
+       return result; 
     }
 }
